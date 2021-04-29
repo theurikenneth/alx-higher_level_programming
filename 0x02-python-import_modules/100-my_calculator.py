0147cf544a69b17359608a51393ebb7927fbcdb6
theurikenneth <theurikenneth@gmail.com>
2021-04-29 09:28:51 +0000

    Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,15 +4,20 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
+    """Throws an error if arguments are not 3"""
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
+    """Defines operator and checks whether it is an operator"""
     ops = {"+": add, "-": sub, "*":mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operator: +, -, * and /")
         sys.exit(1)
 
+    """Casts a and b into integers and assumes all arguments are castable into integers"""
     a = int(sys.argv[1])
     b = int(sys.argv[3])
+
+    """prints a, operator and b as they are arguments 1, 2 and 3 and displays the answer"""
     print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
