f581d037fe513245f87591fc358defb823d276e8
theurikenneth <theurikenneth@gmail.com>
2021-05-06 08:07:12 +0000

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary is None:
-        idct([key, value])
+        dict([key, value])
     else:
         a_dictionary.update({key: value})
     return a_dictionary
