e5bd112fd88feb7446d62346920133c037af88d9
theurikenneth <theurikenneth@gmail.com>
2021-05-17 21:59:43 +0000

    Python - Classes and Objects

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines the square class"""
+
+
+class Square:
+    """Sqaure class with size as private instance attribute"""
+    def __init__(self, size=0):
+        """Initializes the Square"""
+        if type(sie) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns area of the square"""
+        return self.__size * self.__size
