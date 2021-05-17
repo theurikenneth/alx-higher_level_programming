5da19dc029a08e46174da7a607bd12c37e65e5d0
theurikenneth <theurikenneth@gmail.com>
2021-05-17 05:15:54 +0000

    Python - Exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except BaseException as i:
+        print("Exception: {}".format(i), file=sys.stderr)
+        return None
