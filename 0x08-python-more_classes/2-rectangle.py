c43a8ff62efbb61009b7535a7560168ac55c1985
theurikenneth <theurikenneth@gmail.com>
2021-05-24 08:41:32 +0000

    Python - More Classes and Objects

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Contains the rectangle class"""
+
+
+class Rectangle:
+    """A rectangle class"""
+
+    def __inti__(self, width=0, height=0):
+        """Initializes the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width property function"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """width setter"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @propoerty
+    def height(self):
+        """height propoerty function"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """height setter"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.height * self.width
+
+    def permieter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.rectangle == 0 or self.width == 0:
+            return 0
+        return self.height * 2 + self.width * 2
