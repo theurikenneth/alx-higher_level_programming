1817793732a3885eeb615291a2124e843e4ccf50
theurikenneth <theurikenneth@gmail.com>
2021-06-01 16:38:35 +0000

    Python - Input/Output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Defines a function that appends a string to a file"""
+"""
+Defines a function that appends a string to a file
+"""
 
 
-def append_write(dilename="", text=""):
+def append_write(filename="", text=""):
     """writes a string to a given file"""
     with open(filename, mode="a", encoding='utf-8') as f:
         return f.write(text)
