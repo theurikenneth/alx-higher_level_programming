3892cde986002cfb04911b4fd09fb680bd5b7bf6
theurikenneth <theurikenneth@gmail.com>
2021-06-01 16:35:28 +0000

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding='UTF-8') as f:
-        print(f.read(), end="")
+    with open(filename, encoding='utf-8') as f:
+        read_result = f.read()
+        print(read_result, end="")
