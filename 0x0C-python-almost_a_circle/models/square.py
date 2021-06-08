a2677296ca8502d62adb75fb39213967e60356c1
theurikenneth <theurikenneth@gmail.com>
2021-06-08 14:54:03 +0000

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,47 +1,71 @@
 #!/usr/bin/python3
-"""Square class based on Rectangle"""
-from .rectangle import Rectangle
+"""
+...
+"""
+
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class based on Rectangle. No new attributes"""
+    """
+    ...
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(self, size, x, y, id)
+        """
+        ...
+        """
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".\
-            format(self.id, self.x, self.y, self.width)
+        """
+        ...
+        """
+        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
+            self.id, self.x, self.y, self.width
+        )
 
     @property
     def size(self):
+        """
+        ...
+        """
         return self.width
 
     @size.setter
-    def size(self, size):
-        self.width = size
-        self.height = size
+    def size(self, value):
+        """
+        ...
+        """
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """Updates Rectangle attributes. In the following order if positional:
-        id, width, height, x, y
         """
-        numargs = len(args)
-        if numargs > 0:
-            self.id = args[0]
-        if numargs > 1:
-            self.width = args[1]
-            self.height = args[1]
-        if numargs > 2:
-            self.x = args[2]
-        if numargs > 3:
-            self.y = args[3]
-        if numargs == 0:
-            for key, value in kwargs.items():
-                exec("self.{} = {}".format(key, value))
+        ...
+        """
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'size', 'x', 'y']
+
+        if argc > 4:
+            argc = 4
+
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
 
     def to_dictionary(self):
-        """Returns a dictionary of the Rectangle instance's attributes"""
-        newdict = {"id": self.id, "size": self.width,
-                   "x": self.x, "y": self.y}
-        return newdict
+        """
+        ...
+        """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
