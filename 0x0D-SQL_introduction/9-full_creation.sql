27a9cf76d156d1f0895d0fb435df968c431b8d9b
theurikenneth <theurikenneth@gmail.com>
2021-07-12 16:20:25 +0000

    SQL - Introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 -- adding some data
 INSERT INTO second_table
 VALUES
-(1, "John", 10);
-(2, "Alex", 3);
-(3, "Bob", 14);
-(4, "George", 8);
+(1, "John", 10)
+(2, "Alex", 3)
+(3, "Bob", 14)
+(4, "George", 8)
