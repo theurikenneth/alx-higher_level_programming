5795ce13963db70ad58f7304a11be2681723c428
theurikenneth <theurikenneth@gmail.com>
2021-07-13 18:24:31 +0000

    SQL - More queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 -- in the database hbtn_0d_usa
 
 SELECT cities.id, cities.name FROM cities, states
-WHERE cities.state_id=states.id AND states.name="Californi"
+WHERE cities.state_id=states.id AND states.name="California"
 ORDER BY cities.id ASC;
