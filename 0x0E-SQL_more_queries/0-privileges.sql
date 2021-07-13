02f7941608041eace6c8cbe5f697d24c41eef4c1
theurikenneth <theurikenneth@gmail.com>
2021-07-13 18:13:59 +0000

    SQL - More queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- Lists all privileges of the MySQL users
 -- user_0d_1 and user_0d_2 on the localhost Server
 
-SHOW GRANTS FOR 'user_0d_1'@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- list privileges for the second user
-SHOW GRANTS FOR 'user_0d_2'@localhost;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
