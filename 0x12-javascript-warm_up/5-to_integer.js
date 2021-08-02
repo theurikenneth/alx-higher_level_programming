55085e33b7c8d2e10b5a3bb52da7c536c977182f
theurikenneth <theurikenneth@gmail.com>
2021-08-02 16:33:15 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const intRegex = /^[0-9]*?.[0-9]*$/;
 
 if (intRegex.test(process.argv[2])) {
-      console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
