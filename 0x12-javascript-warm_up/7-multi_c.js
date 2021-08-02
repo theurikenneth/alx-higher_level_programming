ce27093d0aadaccabc1ef96d4d12246ee11cbc37
theurikenneth <theurikenneth@gmail.com>
2021-08-02 16:42:30 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let n = parseInt(process.argv[2]);
 if (!isNaN(n)) {
-    for (n--; n >= 0; n--) {
-	console.log('C is fun');
-    }
+  for (n--; n >= 0; n--) {
+    console.log('C is fun');
+  }
 } else {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
