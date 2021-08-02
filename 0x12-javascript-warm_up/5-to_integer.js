38d804385278a1fe551a8948b5c4cc6d31a2e21b
theurikenneth <theurikenneth@gmail.com>
2021-08-02 15:43:30 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
+const intRegex = /^[0-9]*?.[0-9]*$/;
 
-const n = parseInt(process.argv[2]);
-if (!isNaN(n)) {
-    console.log('My number: ' + n);
+if (intRegex.test(process.argv[2])) {
+      console.log('My number: ' + parseInt(process.argv[2]));
 } else {
     console.log('Not a number');
 }
