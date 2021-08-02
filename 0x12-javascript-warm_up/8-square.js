bc192048eecbb1aeebf480434bc2e918bfe26c2c
theurikenneth <theurikenneth@gmail.com>
2021-08-02 16:44:28 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 
 const n = parseInt(process.argv[2]);
 if (!isNaN(n)) {
-    for (let i = 0; i < n; i++) {
-	console.log('X'.repeat(n));
-    }
+  for (let i = 0; i < n; i++) {
+    console.log('X'.repeat(n));
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
