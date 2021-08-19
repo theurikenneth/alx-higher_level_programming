407daab851861ab802d7b5a9abb27067e62faded
theurikenneth <theurikenneth@gmail.com>
2021-08-19 17:57:32 +0000

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # bash script tat takes in an URL, sends a request
 # to that URL and displays the size of the body of the response
 
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
+curl -w '%{size_download}\n' -so /dev/null $1
