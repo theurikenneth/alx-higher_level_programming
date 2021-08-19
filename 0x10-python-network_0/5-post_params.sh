b07437a9f0859482aa62445fa4ca2417fb807b53
theurikenneth <theurikenneth@gmail.com>
2021-08-19 18:19:32 +0000

    Python - Network #0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # scripts that takes in a URL, sends a POST request to the
-# passed URL, and displays the response body
 curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
