f6d675e2660ecb581cc2f93c92512cd60b64a3cc
theurikenneth <theurikenneth@gmail.com>
2021-08-19 18:10:37 +0000

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# bash script tat takes in an URL, sends a request
-# to that URL and displays the size of the body of the response
-
-curl -w '%{size_download}\n' -so /dev/null $1
+# curl and get bytes received
+curl -s -o curl -s -o /dev/null -w '%{size_download}\n' "$1"
