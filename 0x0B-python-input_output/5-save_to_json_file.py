289a08cffa1bcbecab4550b6fed21296cf88fe66
theurikenneth <theurikenneth@gmail.com>
2021-10-21 06:00:47 +0000

    Python - Input/Output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     if filename is None:
         return
-    with open(filename, mode="w", encoding='UTF-8') as f:
-        json_var = json.dump(my_obj, f)
+    with open(filename, "w") as f:
+        f.write(my_obj, f)
